fa64afa92ffc73c558dc70b386d60d5871b213f5
romuri22 <romurimex@gmail.com>
2020-09-18 08:48:39 -0500

    "Pandas"

--- io_utilities.py
+++ io_utilities.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
+# io_utilities.py
+
 def readvanilla():
-    # Reads Dataset and stores results in dictionary
-    # Only for python3.8
+
     data = {"S_L":[],"S_W":[],"P_L":[],"P_W":[],"CL":[]}
     filepath = "data/iris.data"
     with open(filepath,"r") as f:
@@ -15,13 +16,15 @@ def readvanilla():
     return data
 
 
-def read_pandas(filepath):
+if __name__ == "__main__":
+    print(readvanilla())
 
-    df = pd.read_csv(filepath)
-
+def read_pandas(filepath,**kwargs):
+    
+    df = pd.read_csv(filepath,**kwargs)
     return df
 
 if __name__ == "__main__":
-    filepath = "./data/iris.data"
-    df = read_pandas(filepath)
-    print(df)
+    filepath = "data/iris.data"
+    df = read_pandas(filepath=filepath,names=["sepal_length","sepal_width","petal_length","petal_width","class"])
+    print(df.head())
